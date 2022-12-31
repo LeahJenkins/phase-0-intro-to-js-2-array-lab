74959ebb19534829903da7ca537113afcb706960
Leah <leah.jenkins2019@gmail.com>
2022-12-31 15:02:58 -0500

    commit tree 8af1bf2f827f95c0fd4873cbc3bb9ef79cf65e2d

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"
+]
+
+function destructivelyAppendCat(){
+cats.push("Ralph")
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+
+
+function appendCat(){
+    var newArray = cats.slice()
+    newArray.push("Broom")
+    return newArray
+}
+
+function prependCat(){
+    var newArray = cats.slice()
+    newArray.unshift("Arnold")
+    return newArray
+}
+
+function removeLastCat(){
+    var newArray = cats.slice(0, 2)
+    return newArray
+}
+
+function removeFirstCat(){
+    var newArray = cats.slice(1)
+    return newArray
+}
+
+
